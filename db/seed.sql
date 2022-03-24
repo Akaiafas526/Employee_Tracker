47e5184500a59285c000e86d7e25c16f82d7db30
Andreas Kaiafas <akaiafas526@gmail.com>
2022-03-24 15:50:05 -0400

    update seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -8,7 +8,7 @@ VALUES
     ('Finance'),
     ('Legal');
 
-INSERT INTO role
+INSERT INTO roles
     (title, salary, department_id)
 VALUES
     ('Sales Lead', 100000, 1),
@@ -21,7 +21,7 @@ VALUES
     ('Lawyer', 190000, 4);
 
 INSERT INTO employee
-    (first_name, last_name, role_id, manager_id)
+    (first_name, last_name, roles_id, manager_id)
 VALUES
     ('John', 'Doe', 1, NULL),
     ('Mike', 'Chan', 2, 1),
