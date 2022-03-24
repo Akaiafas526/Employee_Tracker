1d674e094a0ed82081e9e5a320e68fb20dfa780a
Andreas Kaiafas <akaiafas526@gmail.com>
2022-03-23 21:49:24 -0400

    created files within organzied folders, transfered readme with instructions

---
+++ Develop/db/connection.js
@@ -0,0 +1,20 @@
+const util = require("util");
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  // Your username
+  user: "root",
+  // Your password
+  password: "password",
+  database: "employees"
+});
+
+// TODO: add callback function for error
+connection.connect();
+
+// Setting up connection.query to use promises instead of callbacks
+// This allows us to use the async/await syntax
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
